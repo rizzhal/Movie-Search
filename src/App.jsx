57fd2ed6f6df4383286ed56e4430c2c72d98cde5
rizzhal <barmanbishal1234@gmail.com>
2026-07-12 01:10:16 +0530

    updated commit: Complete

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,39 @@ function App(){
 
   async function searchMovie() {
     setIsLoading(true)
-    // const apiKey= Your api key
+    setError("")
+    // const apiKey= Your API key
     try {
       const response = await fetch(`http://www.omdbapi.com/?s=${searchInput}&apikey=${apiKey}`)
       if(!response.ok){
         throw new Error(`http status error ${response.status}`)
       }
       const data = await response.json()
-      console.log(data)
-      setMovies(data)
       
+      if(data.Response  === "False"){
+        setError(data.Error)
+        return;
+      }
+      console.log(data)
+      setMovies(data.Search)
     } catch (e) {
-      console.error("Error fetchingData" , e)
-      setError(e)
+      console.error(e.message)
+      setError(e.message)
+    } finally{
       setIsLoading(false)
-    
     }
+   
   }
+     const movieList = movies.map((movie) => {
+        return(
+          <div key={movie.imdbID}>
+            <img src={movie.Poster}/>
+            <h2>{movie.Title}</h2>
+            <p>{movie.Year}</p>
+            <p>{movie.Type}</p>
+          </div>
+        )
+      });
 
   return(
     <div >
@@ -33,8 +49,10 @@ function App(){
        type="text"
        placeholder="search your movie"
        onChange={(e) => setSearchInput(e.target.value)} />
-       <button onClick={searchMovie}>{isLoading ? "Loading...." : "search"}</button>
-
+       <button onClick={searchMovie}>search</button>
+        <p>{isLoading ? "loading..." : ""}</p>
+       <div>{movieList}</div>    
+        {error && <p>{error}</p>}
     </div>
   )
 }
